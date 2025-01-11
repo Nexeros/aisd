af64a852b992faa7b1e90dac27fe1007f0498e13
N04X9 <62847977+Nexeros@users.noreply.github.com>
2025-01-11 15:32:23 +0100

    Add files via upload

---
+++ aisd/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aisd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = textBox1.Text;
+            var huff = new huffman();
+            var freq = huff.CalculateFrequency(input);
+            var root = huff.HuffTree(freq);
+            var codes = new Dictionary<char, String>();
+            String output = "";
+            huff.CodeAsig(root, "", codes);
+            foreach(var c in input)
+            {
+                output += $"{codes[c]}";
+            }
+            String labeltext = output;
+            label1.Text = labeltext;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ aisd/huffman.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aisd
+{
+    public class nodeG
+    {
+        public nodeG left;
+        public nodeG right;
+        public nodeG parent;
+        public int data;
+    }
+    public class nodeGS : nodeG
+    {
+        public char symbol;
+    }
+    internal class huffman
+    {
+        public Dictionary<char, int> CalculateFrequency(string input)
+        {
+            var freq = new Dictionary<char, int>();
+            foreach (char c in input)
+            {
+                if (!freq.ContainsKey(c))
+                {
+                    freq[c] = 0;
+                }
+                freq[c]++;
+            }
+            return freq;
+        }
+
+        public nodeG HuffTree(Dictionary<char, int> freq)
+        {
+            var nodes = new List<nodeG>();
+            foreach (var item in freq)
+            {
+                nodes.Add(new nodeGS { symbol = item.Key, data = item.Value });
+            }
+            while (nodes.Count > 1)
+            {
+                nodes = nodes.OrderBy(n=>n.data).ThenBy(n=>n.GetType()==typeof(nodeGS)?0:1).ToList();
+                var left = nodes[0];
+                var right = nodes[1];
+                var parent = new nodeG
+                {
+                    left = left,
+                    right = right,
+                    data = left.data + right.data,
+                };
+                left.parent = parent;
+                right.parent = parent;
+                nodes.Remove(left);
+                nodes.Remove(right);
+                nodes.Add(parent);
+            }
+            return nodes[0];
+        }
+
+        public void CodeAsig(nodeG node, string code, Dictionary<char, string> codes) 
+        { 
+            if(node == null) return;
+            if(node is nodeGS leaf)
+            {
+                codes[leaf.symbol] = code;
+                Console.WriteLine($"Symbol: {leaf.symbol}, Code: {code}");
+                return;
+            }
+            CodeAsig(node.left, code + "0", codes);
+            CodeAsig(node.right, code + "1", codes);
+        }
+    }
+}
